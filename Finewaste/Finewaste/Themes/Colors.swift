1443afd36a0bc8579d5d1956e584f3571c97583b
Andrean Lay <andreanlay1@gmail.com>
2021-11-01 15:07:32 +0700

    Setup colors constant

--- Finewaste/Finewaste/Themes/Colors.swift
+++ Finewaste/Finewaste/Themes/Colors.swift
@@ -5,4 +5,18 @@
 //  Created by Andrean Lay on 01/11/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct Colors {
+    static let Black = Color("Black")
+    static let White = Color("White")
+    
+    static let DarkGray = Color("DarkGray")
+    static let Gray = Color("Gray")
+    static let GreenOvelay = Color("GreenOverlay")
+    static let Pink = Color("Pink")
+    static let PlaceholderGray = Color("PlaceholderGray")
+    static let Red = Color("Red")
+    static let RedOverlay = Color("RedOverlay")
+    static let Turqoise = Color("Turqoise")
+}
