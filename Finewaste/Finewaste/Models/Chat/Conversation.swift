7aea3f62b60ee60b23c1b819cd24314b1119aa28
Andrean Lay <andreanlay1@gmail.com>
2021-11-25 13:21:13 +0700

    Add some helper computed properties and initializer to conversation model

--- Finewaste/Finewaste/Models/Chat/Conversation.swift
+++ Finewaste/Finewaste/Models/Chat/Conversation.swift
@@ -20,4 +20,32 @@ struct Conversation: Codable, Identifiable {
     var users: [String]
     var lastMessage: String = ""
     var unreadMessages: Int = 0
+    
+    var otherUserName: String {
+        return firstUserId == AuthenticationHelper.shared.userId ? secondUserName : firstUserName
+    }
+    
+    var otherProfilePhotoUrl: String {
+        return firstUserId == AuthenticationHelper.shared.userId ? secondUserPhotoUrl : firstUserPhotoUrl
+    }
+    
+    init(firstUserId: String, firstUserName: String, firstUserPhotoUrl: String, secondUserId: String, secondUserName: String, secondUserPhotoUrl: String, users: [String]) {
+        self.firstUserId = firstUserId
+        self.firstUserName = firstUserName
+        self.firstUserPhotoUrl = firstUserPhotoUrl
+        self.secondUserId = secondUserId
+        self.secondUserName = secondUserName
+        self.secondUserPhotoUrl = secondUserPhotoUrl
+        self.users = users
+    }
+    
+    init() {
+        self.firstUserId = ""
+        self.firstUserName = ""
+        self.firstUserPhotoUrl = ""
+        self.secondUserId = ""
+        self.secondUserName = ""
+        self.secondUserPhotoUrl = ""
+        self.users = []
+    }
 }
