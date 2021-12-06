53d0a90125623e3431d4a1015fa555b469cf2e97
Andrean Lay <andreanlay1@gmail.com>
2021-12-06 22:30:12 +0700

    Fixed Bug #23 where non-owner can post project updates

--- Finewaste/Finewaste/Modules/Project Detail/View/ProjectDetailView.swift
+++ Finewaste/Finewaste/Modules/Project Detail/View/ProjectDetailView.swift
@@ -61,7 +61,7 @@ struct ProjectDetailView: View {
                                 case .about:
                                     ProjectAboutView(viewModel: viewModel)
                                 case .update:
-                                    ProjectUpdateView(projectId: viewModel.projectId)
+                                    ProjectUpdateView(projectId: viewModel.projectId, ownerId: viewModel.projectOwnerId)
                                 default:
                                     EmptyView()
                                 }
