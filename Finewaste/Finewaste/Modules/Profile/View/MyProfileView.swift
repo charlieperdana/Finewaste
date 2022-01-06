ce4f3aa9009e7aeafc30b13c4d3efe399744669a
Andrean Lay <andreanlay1@gmail.com>
2022-01-06 16:45:31 +0700

    Fixed own product info view not visible

--- Finewaste/Finewaste/Modules/Profile/View/MyProfileView.swift
+++ Finewaste/Finewaste/Modules/Profile/View/MyProfileView.swift
@@ -31,6 +31,8 @@ struct MyProfileView: View {
                 NavigationLink(destination: SettingView(), isActive: $showSettingPage) {}
                 
                 DescriptionProfileView(model: model)
+                
+                ProductInfoProfileView(model: model)
             }
             
             
