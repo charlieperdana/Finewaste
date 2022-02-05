1fca9700673a19a7622fe2873aa5b4a066b57e2f
Andrean Lay <andreanlay1@gmail.com>
2022-02-05 18:18:11 +0700

    Handle square image crop for landscape images

--- Finewaste/Finewaste/Extensions/Image+centerWidthCrop.swift
+++ Finewaste/Finewaste/Extensions/Image+centerWidthCrop.swift
@@ -12,10 +12,12 @@ import Kingfisher
 extension KFImage {
     func centerWidthCrop(width: CGFloat = -1, height: CGFloat = -1) -> some View {
         GeometryReader { geo in
+            let smallerSideSize = geo.size.width < geo.size.height ? geo.size.width : geo.size.height
+            
             self
                 .resizable()
                 .scaledToFill()
-                .frame(width: width == -1 ? geo.size.width : width, height: height == -1 ? geo.size.height : height)
+                .frame(width: width == -1 ? smallerSideSize : width, height: height == -1 ? smallerSideSize : height)
                 .clipped()
         }
     }
